86e53ecd619136ee638279a8b4c8914544e82052
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-12 16:23:39 +0100

    add module cadran and combine

---
+++ modules/cadran/cadran.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# module Cadran
+"""
+    Module qui gère les impulsions electriques reçues du cadran rotatif du S63
+    Detecte le chiffre composé sur le cadran en comptant les impulstions
+    Notifie le chiffre composé
+
+    Regles materielles:
+        1 impulsion -> chiffre 1
+        9 impulsions -> chiffre 9
+        10 impulstions -> chiffre 0
+"""
+
+import Constantes
+from threading import Timer
+import time
+if Constantes.IS_RASPBERRY_PI:
+    import RPi.GPIO as GPIO
+else:
+    from modules.test.RPi import GPIO
+
+
+class Cadran:
+    compteur_pulsations = 0
+    numero_compose = ""
+
+    # Timer pour gérer la numérotation d'un chiffre
+    timer_chiffre = None
+    last_input = 0
+
+    notification_prefixe_envoye = False
+
+    def __init__(self):
+        """
+            Initialisation de la PIN du Raspberry reliée au cadran du S63
+        """
+        # Set GPIO mode to Broadcom SOC numbering
+        GPIO.setmode(GPIO.BCM)
+
+        # Configuration du GPIO pour écouter les mouvements du cadran
+        # On utilise un "pull up" pour forcer l'état haut quand le circuit
+        # du cadran est ouvert
+        # A chaque changement d'état la callback CompteImpulsions est appelée
+        GPIO.setup(Constantes.PIN_CADRAN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(Constantes.PIN_CADRAN, GPIO.BOTH,
+                              callback=self.CompteImpulsions)
+
+    # Enregistrement des callbacks
+    def RegisterCallback(self, NotificationChiffre):
+        """
+            Enregistrement de la callbacks utilisée pour notifier quand
+            un chiffre est composé sur le cadran
+        """
+        self.NotificationChiffre = NotificationChiffre
+
+    def CompteImpulsions(self):
+        """
+            Compte les impulsions reçues et arme un timer de fin
+            Une impulsion est définie par une transition sur l'entrée PIN
+            La fonction de callback est executée dans un thread séparé
+            instancié par RPi.GPIO
+        """
+        input = GPIO.input(Constantes.PIN_CADRAN)
+        if input and not self.last_input:
+            self.compteur_pulsations += 1
+
+            if self.timer_chiffre is not None:
+                self.timer_chiffre.cancel()
+
+            self.timer_chiffre = Timer(Constantes.TIMOUT_CHIFFRE_CADRAN,
+                                       self.FinNumerotationChiffre)
+            self.timer_chiffre.start()
+        self.last_input = input
+        time.sleep(Constantes.TEMPO_ENTRE_IMPULSIONS)
+
+    # Quand la numérotation d'un chiffre est terminée
+    def FinNumerotationChiffre(self):
+        """
+            Appelé par le timer quand la numérotation d'un chiffre est finie
+            Envoie une notification avec le chiffre composé
+        """
+        if self.compteur_pulsations == 10:
+            self.compteur_pulsations = 0
+        self.numero_compose += str(self.compteur_pulsations)
+        self.NotificationChiffre(self.compteur_pulsations)
+        self.compteur_pulsations = 0
