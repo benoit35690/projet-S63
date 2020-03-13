6ff0808b5a7a76839267764f2a531b042b785ba8
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-13 16:16:52 +0100

    aa

--- modules/automate/automate.py
+++ modules/automate/automate.py
@@ -27,9 +27,6 @@ class Automate:
             NotificationRaccroche=self.ReceptionRaccroche),
             # NotificationVerifDecroche=self.ReceptionVerifDecroche)
 
-        while fonctionnement_automate:
-            # do nothing
-
     def ReceptionChiffre(self, chiffre):
         print ("[Automate ReceptionChiffre] Chiffre recu = ", chiffre)
 
