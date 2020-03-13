4155fef8672aceb4edcf41c7c222f9740bff00bc
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-13 16:20:53 +0100

    aaa

--- modules/automate/automate.py
+++ modules/automate/automate.py
@@ -15,17 +15,13 @@ class Automate:
     Combine = None
 
     def __init__(self):
-#        signal.signal(signal.SIGINT, self.OnSignal)
-        fonctionnement_automate = True
-
         self.Cadran = Cadran()
         self.Combine = Combine()
 
         self.Cadran.RegisterCallback(NotificationChiffre=self.ReceptionChiffre)
         self.Combine.RegisterCallback(
             NotificationDecroche=self.ReceptionDecroche,
-            NotificationRaccroche=self.ReceptionRaccroche),
-            # NotificationVerifDecroche=self.ReceptionVerifDecroche)
+            NotificationRaccroche=self.ReceptionRaccroche)
 
     def ReceptionChiffre(self, chiffre):
         print ("[Automate ReceptionChiffre] Chiffre recu = ", chiffre)
@@ -43,7 +39,6 @@ class Automate:
         print "[Automate Exit]"
         self.Cadran.ArretDetectionImpulsions()
         self.Combine.ArretVerificationDecroche()
-        self.fonctionnement_automate = False
 
     def OnSignal(self, signal, frame):
         print "[Automate SIGNAL] Shutting down on %s" % signal
