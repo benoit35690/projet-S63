a6fdcebabbc1e5410986a2ed027a21fc68fbc88d
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-22 17:13:25 +0100

    aa

--- Automate_S63.py
+++ Automate_S63.py
@@ -14,13 +14,15 @@ callback_queue = Queue.Queue()
 
 
 class Message:
-    message_type = 0
-    chiffre = 0
+    transition_automate = 0
+    chiffre_compose     = 0
+    numero_compose      = 0
 
 
 class Automate_S63:
     dial_number = ""
     automate_actif = True
+    etat_automate = Constantes.ETAT_INIT
     offHook = False
     offHookTimeoutTimer = None
 
@@ -28,6 +30,8 @@ class Automate_S63:
         print ("[Automate_S63 __init__]")
 
         signal.signal(signal.SIGINT, self.OnSignal)
+
+        self.etat_automate = Constantes.ETAT_INIT
 
         # creation de la queue de messages
         self.message_queue = Queue.Queue(maxsize=0)
@@ -51,34 +55,31 @@ class Automate_S63:
     def ReceptionChiffre(self, chiffre):
         #print ("[Automate ReceptionChiffre] Chiffre recu = ", chiffre)
         message = Message()
-        message.message_type = Constantes.MESSAGE_CHIFFRE
-        message.chiffre = chiffre
+        message.transition_automate = Constantes.TRANSITION_CHIFFRE_COMP
+        message.chiffre_compose = chiffre
         self.message_queue.put(message)
 
     def ReceptionDecroche(self):
         #print ("[Automate ReceptionDecroche]")
         message = Message()
-        message.message_type = Constantes.MESSAGE_DECROCHE
-        message.chiffre = 0
+        message.transition_automate = Constantes.TRANSITION_DECROCHE
         self.message_queue.put(message)
 
     def ReceptionRaccroche(self):
         #print ("[Automate ReceptionRaccroche]")
         message = Message()
-        message.message_type = Constantes.MESSAGE_RACCROCHE
-        message.chiffre = 0
+        message.transition_automate = Constantes.TRANSITION_RACCROCHE
         self.message_queue.put(message)
 
     def ReceptionVerifDecroche(self, etat):
         #print ("[Automate ReceptionVerifDecroche]", etat)
         message = Message()
-        message.chiffre = 0
         if etat == GPIO.HIGH:
             #print("[Combine VerifieCombine] HIGH -> Raccroche")
-            message.message_type = Constantes.MESSAGE_RACCROCHE
+            message.transition_automate = Constantes.TRANSITION_RACCROCHE
         else:
             #print("[Combine VerifieCombine] LOW -> Decroche")
-            message.message_type = Constantes.MESSAGE_DECROCHE
+            message.transition_automate = Constantes.TRANSITION_DECROCHE
         self.message_queue.put(message)
 
     def FonctionWorkerThread(self):
@@ -105,6 +106,59 @@ class Automate_S63:
 
     def TraiteMessage(self, message):
         print ("[Automate TraiteMessage] message_type=",
+               message.message_type)
+        if message == Constantes.TRANSITION_RACCROCHE:
+            self.TraiteTransitionRaccroche(message)
+        elif message == Constantes.TRANSITION_DECROCHE:
+            self.TraiteTransitionDecroche(message)
+        elif message == Constantes.TRANSITION_APPEL_ENTRANT:
+            self.TraiteTransitionAppelEntrant(message)
+        elif message == Constantes.TRANSITION_FIN_APPEL:
+            self.TraiteTransitionFinAppel(message)
+        elif message == Constantes.TRANSITION_TIMER_OUBLIE:
+            self.TraiteTransitionTimerOublie(message)
+        elif message == Constantes.TRANSITION_ECHEC_NUM:
+            self.TraiteTransitionEchecNumerotation(message)
+        elif message == Constantes.TRANSITION_CHIFFRE_COMP:
+            self.TraiteTransitionChiffreCompose(message)
+        elif message == Constantes.TRANSITION_NUMERO:
+            self.TraiteTransitionNumeroCompose(message)
+        else:
+            print ("[Automate TraiteMessage] MESSAGE INCONNU message_type=",
+                   message.message_type)
+
+    def TraiteTransitionRaccroche(self, message):
+        print ("[Automate TraiteTransitionRaccroche] message_type=",
+               message.message_type)
+
+
+
+    def TraiteTransitionDecroche(self, message):
+        print ("[Automate TraiteTransitionDecroche] message_type=",
+               message.message_type)
+
+    def TraiteTransitionAppelEntrant(self, message):
+        print ("[Automate TraiteTransitionAppelEntrant] message_type=",
+               message.message_type)
+
+    def TraiteTransitionFinAppel(self, message):
+        print ("[Automate TraiteTransitionFinAppel] message_type=",
+               message.message_type)
+
+    def TraiteTransitionTimerOublie(self, message):
+        print ("[Automate TraiteTransitionTimerOublie] message_type=",
+               message.message_type)
+
+    def TraiteTransitionEchecNumerotation(self, message):
+        print ("[Automate TraiteTransitionEchecNumerotation] message_type=",
+               message.message_type)
+
+    def TraiteTransitionChiffreCompose(self, message):
+        print ("[Automate TraiteTransitionChiffreCompose] message_type=",
+               message.message_type)
+
+    def TraiteTransitionNumeroCompose(self, message):
+        print ("[Automate TraiteTransitionNumeroCompose] message_type=",
                message.message_type)
 
     def OnSignal(self, signal, frame):
--- Constantes.py
+++ Constantes.py
@@ -5,27 +5,43 @@
 """
 
 # liste des PIN du Raspberry
-PIN_CADRAN             = 4
-PIN_COMBINE            = 3
-PIN_SOLENOIDE_GAUCHE   = 1
-PIN_SOLENOIDE_DROIT    = 2
-PIN_COMBINE_ANTIREBOND = 100
-
+PIN_CADRAN               = 4
+PIN_COMBINE              = 3
+PIN_SOLENOIDE_GAUCHE     = 1
+PIN_SOLENOIDE_DROIT      = 2
+PIN_COMBINE_ANTIREBOND   = 100
 
 # aprés 0.9s on considère que le cadran a fini de numéroter le chiffre
-TIMOUT_CHIFFRE_CADRAN  = 0.9
-TEMPO_ENTRE_IMPULSIONS = 0.002
+TIMOUT_CHIFFRE_CADRAN    = 0.9
+TEMPO_ENTRE_IMPULSIONS   = 0.002
 
-TIMER_COMBINE          = 2
-TIMEOUT_VERIF_COMBINE  = 1
+TIMER_COMBINE            = 2
+TIMEOUT_VERIF_COMBINE    = 1
 
-TIMEOUT_AUTOMATE       = 0.5
+TIMEOUT_AUTOMATE         = 0.5
 
-# liste des type de message de la queue de messages
-MESSAGE_DECROCHE       = 1
-MESSAGE_RACCROCHE      = 2
-MESSAGE_CHIFFRE        = 3
+# etats du combiné
+COMBINE_DECROCHE         = 1
+COMBINE_RACCROCHE        = 2
 
 # liste des états de l'automate
+ETAT_INIT                = 0
+ETAT_REPOS               = 1
+ETAT_DECROCHE_REPOS      = 2
+ETAT_DECROCHE_OUBLIE     = 3
+ETAT_SONNERIE            = 4
+ETAT_APPEL_ENTRANT       = 5
+ETAT_NUMEROTATION        = 6
+ETAT_APPEL_1             = 7
+ETAT_APPEL_2             = 8
 
 # liste des transitions de l'automate
+TRANSITION_INIT          = 0
+TRANSITION_RACCROCHE     = 1
+TRANSITION_DECROCHE      = 2
+TRANSITION_APPEL_ENTRANT = 3
+TRANSITION_FIN_APPEL     = 4
+TRANSITION_TIMER_OUBLIE  = 5
+TRANSITION_ECHEC_NUM     = 6
+TRANSITION_CHIFFRE_COMP  = 7
+TRANSITION_NUMERO        = 8
