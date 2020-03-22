72f288730d3d8b751f195e40783db5c34fe7a000
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-22 16:15:08 +0100

    aa

--- Automate_S63.py
+++ Automate_S63.py
@@ -1,12 +1,12 @@
-# import os
+import os
 import Queue
 from threading import Thread
-# import threading
+import threading
 import signal
 import sys
 import RPi.GPIO as GPIO
 import Constantes
-# from threading import Timer
+from threading import Timer
 from modules.Cadran import Cadran
 from modules.Combine import Combine
 
