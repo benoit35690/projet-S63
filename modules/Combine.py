f6cb4b153d5bf1b57d43f56018f2747bb6fd3674
benoit35690 <benoit.gruson@yahoo.fr>
2020-03-22 12:29:21 +0100

    aa

--- modules/Combine.py
+++ modules/Combine.py
@@ -1,65 +1,84 @@
-# Rotary Dial Parser
-# Expects the following hardware rules:
-# 1 is 1 pulse
-# 9 is 9 pulses
-# 0 is 10 pulses
+# -*- coding: utf-8 -*-
+# module Combine
+"""
+    Module qui gère le contact electrique reçue de l'interrupteur du combine
+    du S63
+    Notifie l'état du combiné
+"""
 
+import Constantes
 import RPi.GPIO as GPIO
 from threading import Timer
 import time
 
+
 class Combine:
-
-    # We'll be reading on/off hook events from BCM GPIO 3
-    pin_onhook = 3
-
     # Timer to ensure we're on hook
     onhook_timer = None
     should_verify_hook = True
 
     def __init__(self):
+        """
+            Initialisation de la PIN du Raspberry reliée au combiné du S63
+        """
+        print ("[Combine __init__]")
+
         # Set GPIO mode to Broadcom SOC numbering
         GPIO.setmode(GPIO.BCM)
 
-        # Listen for on/off hooks
-        GPIO.setup(self.pin_onhook, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.pin_onhook, GPIO.BOTH, callback = self.HookEvent, bouncetime=100)
+        # Configuration du GPIO pour écouter les mouvements du cadran
+        # On utilise un "pull up" pour forcer l'état haut quand l'interrupteur
+        # du combiné est ouvert
+        # pour éviter d'être notifié plusieurs fois par évenement, on définie
+        # un temps d'anti-rebond de 100 ms
+        # A chaque changement d'état la callback EvenementDecroche est appelée
+        GPIO.setup(Constantes.PIN_COMBINE, GPIO.IN,
+                   pull_up_down=GPIO.PUD_UP)
 
-        self.onhook_timer = Timer(2, self.verifyHook)
-        self.onhook_timer.start()
+        GPIO.add_event_detect(Constantes.PIN_COMBINE, GPIO.BOTH,
+                              callback=self.EvenementDecroche,
+                              bouncetime=Constantes.PIN_COMBINE_ANTIREBOND)
 
-    # Wrapper around the off/on hook event
-    def HookEvent(self, channel):
-        input = GPIO.input(self.pin_onhook)
+#        self.onhook_timer = Timer(2, self.verifyHook)
+#        self.onhook_timer.start()
+
+    def EvenementDecroche(self, channel):
+        print ("[Combine EvenementDecroche]")
+        input = GPIO.input(Constantes.PIN_COMBINE)
         if input:
-            self.hook_state = 1
-            self.OnHookCallback()
+            self.etat_decroche = 1
+            self.NotificationRaccroche()
         else:
-            self.hook_state = 0
-            self.OffHookCallback()
-        #self.OnHookCallback()
+            self.etat_decroche = 0
+            self.NotificationDecroche()
 
-    def StopVerifyHook(self):
-        print("[RotaryDial StopVerifyHook]", input)
-        self.should_verify_hook = False
+#    def StopVerifyHook(self):
+#        print("[RotaryDial StopVerifyHook]", input)
+#        self.should_verify_hook = False
 
-    def verifyHook(self):
-        while self.should_verify_hook:
-            state = GPIO.input(self.pin_onhook)
-            #if state == GPIO.HIGH:
-            #    print("[RotaryDial verifyHook] HIGH")
-            #else:
-            #    print("[RotaryDial verifyHook] LOW")
-            self.OnVerifyHook(state)
-            time.sleep(1)
+#    def verifyHook(self):
+#        while self.should_verify_hook:
+#            state = GPIO.input(self.pin_onhook)
+#            #if state == GPIO.HIGH:
+#            #    print("[RotaryDial verifyHook] HIGH")
+#            #else:
+#            #    print("[RotaryDial verifyHook] LOW")
+#            self.OnVerifyHook(state)
+#            time.sleep(1)
 
-
-    def RegisterCallback(self, OffHookCallback, OnHookCallback, OnVerifyHook):
-        self.OffHookCallback = OffHookCallback
-        self.OnHookCallback = OnHookCallback
-        self.OnVerifyHook = OnVerifyHook
-        input = GPIO.input(self.pin_onhook)
+    # Enregistrement des callbacks
+    def RegisterCallback(self, NotificationDecroche, NotificationRaccroche):
+    #                     NotificationVerifDecroche):
+        """
+            Enregistrement de la callbacks utilisée pour notifier quand
+            l'état du combiné change
+        """
+        print ("[Combine RegisterCallback]")
+        self.NotificationDecroche = NotificationDecroche
+        self.NotificationRaccroche = NotificationRaccroche
+#        self.NotificationVerifDecroche = NotificationVerifDecroche
+        input = GPIO.input(Constantes.PIN_COMBINE)
         if input:
-            self.OffHookCallback()
+            self.NotificationDecroche()
         else:
-            self.OnHookCallback()
+            self.NotificationRaccroche()
