8781666a036622822c9fde49c4120d9a15a15221
benoit35690 <benoit.gruson@yahoo.fr>
2020-04-23 10:42:09 +0200

    aa

--- modules/Tonalite.py
+++ modules/Tonalite.py
@@ -14,42 +14,45 @@ class Tonalite:
         print "[Tonalite] __init__"
 
     def startLecture(self, fichier):
-        if self.lectureActive == 1:
+        print "[Tonalite] startLecture"
+        if self.lectureActive is not None:
+            print "[Tonalite] startLecture lecture en cours"
             self.stopLecture()
 
         self.lectureActive = 1
         self.fichierTonalite = fichier
         if self.timerLecture is not None:
-            print "[RINGTONE] Handset already playing?"
+            print "[Tonalite] Handset already playing?"
             return
 
         self.timerLecture = Timer(0, self.lecture)
         self.timerLecture.start()
 
     def stopLecture(self):
+        print "[Tonalite] stopLecture"
         self.lectureActive = 0
         if self.timerLecture is not None:
             self.timerLecture.cancel()
             self.timerLecture = None
 
     def lecture(self):
-        print "[Tonalite] play"
+        print "[Tonalite] lecture"
 
         wf = wave.open(self.fichierTonalite, 'rb')
         p = pyaudio.PyAudio()
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
 
         while self.lectureActive:
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
             data = wf.readframes(Constantes.AUDIO_CHUNK)
             while data != '' and self.lectureActive:
                 stream.write(data)
                 data = wf.readframes(Constantes.AUDIO_CHUNK)
+            print "[Tonalite] lecture rebouclage"
+            stream.stop_stream()
+            stream.close()
             wf.rewind()
-
-        stream.stop_stream()
-        stream.close()
-        wf.rewind()
-        wf.close()
+            wf.close()
+        print "[Tonalite] lecture fin de procedure"
